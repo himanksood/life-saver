6005be14ddc14768d8369ee4fdf48415b6595b41
himank sood <himank.sood93@gmail.com>
2017-07-04 12:47:09 +0530

    new changes

--- Home1/app/src/androidTest/java/com/example/dell/home1/ApplicationTest.java
+++ Home1/app/src/androidTest/java/com/example/dell/home1/ApplicationTest.java
@@ -11,3 +11,31 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 }
+import java.io.*;
+import java.net.*;
+public class GossipServer
+{
+	public static void main (String [] args) throws Exception
+	{
+	    ServerSocket sersock= new ServerSocket(3000);//used to communcate with the client
+			System.out.println("Server ready for chatting");
+				Socket sock=sersock.accept();// waiting fr a client to listen
+			BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));// sending to client 
+    OutputStream otream=sock.getOutputStream();
+		PrintWriter pwrite = new PrintWriter(otream,true);// recieving frm server
+			InputStream itream= sock.getInputStream();
+					BufferedReader receiveread = new BufferedReader(new InputStreamReader (itream)); //recieving  frm client to recieve messages
+						String receiveMessage,sendMessage;
+			              while(true)
+					{  
+				  if((receiveMessage=receiveread.readLine()) !=null) // is not equal to anything	 
+				  {
+					  System.out.println(receiveMessage);
+				  }
+						sendMessage=keyRead.readLine();
+							pwrite.println(sendMessage);
+								pwrite.flush();
+					}
+		
+	}
+}
